5669f9fd8d44e842b754e8f5e9b866c1b6751bd9
Daniel Lopez <daniel.lopez@smartick.es>
2013-07-09 22:58:02 +0000

    web reads from index.html

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+Var fs =require('fs');
+
+var contents = function(name){
+    return fs.readFileSync(name, "utf8");
+}
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(contents('index.html');
 });
 
 var port = process.env.PORT || 5000;
